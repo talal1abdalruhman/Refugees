642fdc4f42022e7decbf54840f26ef5ed7138f51
Talal Albalawneh <talal1abdalruhman@users.noreply.github.com>
2020-10-26 17:50:56 +0300

    finish work with registration + searchable

--- app/src/main/java/com/example/refugees/Signup.java
+++ app/src/main/java/com/example/refugees/Signup.java
@@ -1,5 +1,7 @@
 package com.example.refugees;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.NotificationCompat;
@@ -8,7 +10,13 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
@@ -16,7 +24,27 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
+import com.example.refugees.HelperClasses.Address;
+import com.example.refugees.HelperClasses.User;
 import com.example.refugees.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Signup extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "language";
@@ -29,6 +57,17 @@ public class Signup extends AppCompatActivity {
     ImageView top;
     ScrollView form_signup;
     ConstraintLayout warning;
+
+    private static final int GALLERY_REQUEST_CODE = 1;
+    private String ADD_USER_TAG = "userRegister";
+    private TextInputEditText fName, eMail, phoneNo, password, gover, city;
+    private String textName, textEmail, textPhone, textPassword, textGovern, textCity;
+    private CircleImageView profileImg;
+    private Uri imageUri = null;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +82,7 @@ public class Signup extends AppCompatActivity {
                 setup();
             }
         });
+        InitializeFields();
         top = findViewById(R.id.top);
         bottom_dark = findViewById(R.id.bottom_dark);
         bottom_light = findViewById(R.id.bottom_light);
@@ -112,7 +152,8 @@ public class Signup extends AppCompatActivity {
         overridePendingTransition(0, 0);
     }
 
-    public void register(View view) {
+    // Register a new user methods
+    public void showSearchable(String userId) {
         int duration = 500;
         float mul = -3;
         int delay = 0;
@@ -124,6 +165,7 @@ public class Signup extends AppCompatActivity {
             public void onAnimationEnd(Animator animation) {
                 Intent intent = new Intent(context, Searchable.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                intent.putExtra("user_id", userId);
                 startActivity(intent);
                 finish();
             }
@@ -131,5 +173,156 @@ public class Signup extends AppCompatActivity {
 
     }
 
+    public void InitializeFields() {
+
+        mStorageRef = FirebaseStorage.getInstance().getReference().child("ProfileImages");
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference().child("users");
+
+        fName = findViewById(R.id.register_full_name);
+        eMail = findViewById(R.id.register_email);
+        phoneNo = findViewById(R.id.register_phone);
+        password = findViewById(R.id.register_password);
+        gover = findViewById(R.id.register_govrnator);
+        city = findViewById(R.id.register_City);
+        profileImg = findViewById(R.id.register_img);
+    }
+
+    public void RegisterUser(View view) {
+        textName = fName.getText().toString();
+        textEmail = eMail.getText().toString();
+        textPhone = phoneNo.getText().toString();
+        textPassword = password.getText().toString();
+        textGovern = gover.getText().toString();
+        textCity = city.getText().toString();
+
+        VerifyUserByEmail();
+    }
+
+    private void VerifyUserByEmail() {
+
+        if (CheckValidation()) {
+            Log.d(ADD_USER_TAG, "VerifyUserByEmail start");
+            mAuth.createUserWithEmailAndPassword(textEmail, textPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(ADD_USER_TAG, "Auth success");
+
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(ADD_USER_TAG, "verification email sent");
+                                    UploadUserInfo();
+                                } else {
+                                    Log.d(ADD_USER_TAG, "verification email NOT sent");
+                                }
+                            }
+                        });
+
+                    } else {
+                        Log.d(ADD_USER_TAG, "Error: " + task.getException().getMessage());
+                    }
+                }
+            });
+        } else {
+            Log.d(ADD_USER_TAG, "Error: NOT Valid");
+        }
+    }
+
+    private boolean CheckValidation() {
+        if (imageUri != null && !textName.isEmpty()
+                && !textEmail.isEmpty() && !textPhone.isEmpty()
+                && !textPassword.isEmpty() && !textGovern.isEmpty()
+                && !textCity.isEmpty())
+            return true;
+        return false;
+    }
+
+    private void UploadUserInfo() {
+        if (CheckValidation()) {
+            final String userId = mAuth.getCurrentUser().getUid();
+            StorageReference imgRef = mStorageRef.child(userId + ".jpg");
+            imgRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    taskSnapshot.getStorage().getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(ADD_USER_TAG, "Img upload success");
+                                String downloadUrl = task.getResult().toString();
+                                Address address = new Address(textGovern, textCity);
+                                User user = new User(
+                                        userId, downloadUrl,
+                                        textName, textEmail,
+                                        textPhone, textPassword,
+                                        address);
+                                mDatabaseReference.child(userId).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(ADD_USER_TAG, "user info upload success");
+                                        showSearchable(userId);
+                                    }
+                                });
+
+                            } else {
+                                Log.d(ADD_USER_TAG, "Error: " + task.getException().getMessage());
+                            }
+                        }
+                    });
+                }
+            });
+        } else {
+            Log.d(ADD_USER_TAG, "Error: NOT Valid");
+        }
+
+    }
+
+    public void PickImage(View view) {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                imageUri = result.getUri();
+
+                try {
+                    if (Build.VERSION.SDK_INT < 28) {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(
+                                this.getContentResolver(),
+                                imageUri);
+                        profileImg.setImageBitmap(bitmap);
+                    } else {
+                        ImageDecoder.Source src = ImageDecoder.createSource(this.getContentResolver(), imageUri);
+                        Bitmap bitmap = ImageDecoder.decodeBitmap(src);
+                        profileImg.setImageBitmap(bitmap);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
