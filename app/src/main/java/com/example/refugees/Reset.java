7d6f041e32522c51768970f06559b17c0788b0cb
Talal Albalawneh <talal1abdalruhman@users.noreply.github.com>
2020-10-30 18:21:39 +0200

    finish work with reset password

--- app/src/main/java/com/example/refugees/Reset.java
+++ app/src/main/java/com/example/refugees/Reset.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewPropertyAnimator;
@@ -12,10 +13,23 @@ import android.view.ViewTreeObserver;
 import android.view.animation.Interpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
+
+import com.example.refugees.HelperClasses.Validation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
 
 public class Reset extends AppCompatActivity {
     Context context = this;
@@ -23,7 +37,13 @@ public class Reset extends AppCompatActivity {
     ImageView bottom_dark;
     ImageView bottom_light;
     LinearLayout form;
-    Interpolator interpolator = new FastOutSlowInInterpolator() ;
+    Interpolator interpolator = new FastOutSlowInInterpolator();
+
+    FirebaseAuth mAuth;
+    TextView msg;
+    TextInputLayout email;
+    LinearLayout innerLayout;
+    CircularProgressButton sendBtn;
 
     int duration = 400;
     float ScreenWidth;
@@ -38,7 +58,7 @@ public class Reset extends AppCompatActivity {
         setContentView(R.layout.activity_reset);
         final ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.background);
         ViewTreeObserver vto = layout.getViewTreeObserver();
-        vto.addOnGlobalLayoutListener (new ViewTreeObserver.OnGlobalLayoutListener() {
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
                 layout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
@@ -55,7 +75,14 @@ public class Reset extends AppCompatActivity {
         bottom_light = findViewById(R.id.bottom_light);
         bottom_dark = findViewById(R.id.bottom_dark);
         form = findViewById(R.id.reset);
+
+        mAuth = FirebaseAuth.getInstance();
+        msg = findViewById(R.id.reset_txt);
+        email = findViewById(R.id.reset_email);
+        innerLayout = findViewById(R.id.reset_inner_linear);
+        sendBtn = findViewById(R.id.reset_send_btn);
     }
+
     public void setup() {
         form.setX(ScreenWidth * direction);
         bottom_dark.setY(ScreenHeight - bottom_dark.getHeight());
@@ -68,26 +95,30 @@ public class Reset extends AppCompatActivity {
         top.setScaleY(0.2f);
         animate();
     }
+
     public ViewPropertyAnimator animate() {
-        return form.animate().setDuration(duration).translationXBy((ScreenWidth/2 + form.getWidth()/2f) * -1 * direction).setInterpolator(interpolator);
+        return form.animate().setDuration(duration).translationXBy((ScreenWidth / 2 + form.getWidth() / 2f) * -1 * direction).setInterpolator(interpolator);
     }
+
     public ViewPropertyAnimator animate(int next) {
         return form.animate().setDuration(duration).translationXBy(form.getWidth() * -1 * direction * next).setInterpolator(interpolator);
     }
+
     public void click(View view) {
         animate(direction).setListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
-                Intent intent = intent = new Intent(context,  Signup.class);;
+                Intent intent = intent = new Intent(context, Signup.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivity(intent);
                 finish();
             }
         });
     }
+
     @Override
     public void onBackPressed() {
-        if(pressed)
+        if (pressed)
             return;
         pressed = true;
         animate(direction).setListener(new AnimatorListenerAdapter() {
@@ -103,9 +134,36 @@ public class Reset extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public void onPause() {
         super.onPause();
         overridePendingTransition(0, 0);
+    }
+
+    public void SendResetEmail(View view) {
+        email.setError(null);
+        email.setErrorEnabled(false);
+        Validation validator =  new Validation(getResources());
+        String emailAddress = email.getEditText().getText().toString();
+        if(!validator.validateLoginEmail(email)) return;
+        sendBtn.startAnimation();
+        mAuth.sendPasswordResetEmail(emailAddress).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    msg.setText(getResources().getString(R.string.we_send_you_link_to_reset_your_password_nplease_check_your_email_address_n) + emailAddress);
+                    sendBtn.startAnimation();
+                    innerLayout.setVisibility(View.GONE);
+                    msg.setVisibility(View.VISIBLE);
+                    sendBtn.revertAnimation();
+                } else if (task.getException() instanceof FirebaseAuthInvalidUserException){
+                    sendBtn.revertAnimation();
+                    sendBtn.setBackground(getDrawable(R.drawable.login_btn_bg));
+                    email.setError(getString(R.string.email_address_not_register));
+                    email.requestFocus();
+                }
+            }
+        });
     }
 }
