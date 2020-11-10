e067166309b09334eb8341f234d1331261cb055d
Talal Albalawneh <talal1abdalruhman@users.noreply.github.com>
2020-11-10 22:35:54 +0200

    Settings fragment, change language, update email

--- app/src/main/java/com/example/refugees/MainScreenFragments/SettingsFragment.java
+++ app/src/main/java/com/example/refugees/MainScreenFragments/SettingsFragment.java
@@ -4,8 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.refugees.R;
 
@@ -14,15 +21,70 @@ public class SettingsFragment extends Fragment {
     public SettingsFragment() {
         // Required empty public constructor
     }
+
     View view;
+    public static ConstraintLayout langLayout, chooseLangLayout, changeEmailLayout;
+    RadioButton radioArabic, radioEnglish;
+    ImageView emailArrow, passwordArrow;
+    public static ImageView arrow;
+    public static TextView selectedLang;
+    boolean isOpen = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_settings, container, false);
-
-
         // Inflate the layout for this fragment
         return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        langLayout = view.findViewById(R.id.settings_lang_layout);
+        chooseLangLayout = view.findViewById(R.id.settings_choose_lang_layout);
+        arrow = view.findViewById(R.id.settings_lang_down_arrow);
+        selectedLang = view.findViewById(R.id.settings_selected_lang);
+        radioArabic = view.findViewById(R.id.radioButton_arabic);
+        radioEnglish = view.findViewById(R.id.radioButton_english);
+        emailArrow = view.findViewById(R.id.arrow_email);
+        passwordArrow = view.findViewById(R.id.arrow_password);
+        changeEmailLayout = view.findViewById(R.id.changeEmailLayout);
+        String language = getResources().getConfiguration().locale.getLanguage();
+
+        if(language.equals("en")){
+            radioEnglish.setChecked(true);
+            selectedLang.setText(getString(R.string.english));
+        } else {
+            radioArabic.setChecked(true);
+            selectedLang.setText(getString(R.string.arabic));
+            emailArrow.setImageDrawable(getResources().getDrawable(R.drawable.ic_arrow_left));
+            passwordArrow.setImageDrawable(getResources().getDrawable(R.drawable.ic_arrow_left));
+        }
+
+        langLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isOpen) {
+                    chooseLangLayout.setVisibility(View.VISIBLE);
+                    arrow.setImageDrawable(getResources().getDrawable(R.drawable.ic_arrow_up));
+                    isOpen = true;
+                } else {
+                    chooseLangLayout.setVisibility(View.GONE);
+                    arrow.setImageDrawable(getResources().getDrawable(R.drawable.ic_arrow_down));
+                    isOpen = false;
+                }
+            }
+        });
+
+        changeEmailLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Navigation.findNavController(view).navigate(R.id.action_settings_to_updateEmailFragment);
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/com/example/refugees/MainScreenFragments/UpdateEmailFragment.java
@@ -0,0 +1,106 @@
+package com.example.refugees.MainScreenFragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.refugees.HelperClasses.Validation;
+import com.example.refugees.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
+
+public class UpdateEmailFragment extends Fragment {
+
+    public UpdateEmailFragment() {
+        // Required empty public constructor
+    }
+
+    CircularProgressButton updateBtn;
+    FirebaseUser currentUser;
+    TextInputLayout passwordLayout, newEmailLayout;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update_email, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        updateBtn = view.findViewById(R.id.settings_update_email);
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        passwordLayout = view.findViewById(R.id.updateEmail_layout_password);
+        newEmailLayout = view.findViewById(R.id.updateEmail_layout_email);
+        Validation validator = new Validation(getResources());
+
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passwordLayout.setError(null);
+                passwordLayout.setErrorEnabled(false);
+                if(currentUser != null){
+                    if(!validator.validateNotEmpty(passwordLayout) | !validator.validateEmail(newEmailLayout))
+                        return;
+                    updateBtn.startAnimation();
+                    String password = passwordLayout.getEditText().getText().toString().trim();
+                    AuthCredential credential = EmailAuthProvider.getCredential(currentUser.getEmail(), password);
+                    currentUser.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                String newEmail = newEmailLayout.getEditText().getText().toString().trim();
+                                currentUser.updateEmail(newEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()){
+                                            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users");
+                                                    userRef.child(currentUser.getUid()).child("email").setValue(newEmail)
+                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            updateBtn.revertAnimation();
+                                                            updateBtn.setBackground(getResources().getDrawable(R.drawable.login_btn_bg));
+                                                            Navigation.findNavController(view).navigate(R.id.action_updateEmailFragment_to_settings);
+                                                        }
+                                                    });
+                                        }
+                                    }
+                                });
+                            } else if(task.getException() instanceof FirebaseAuthInvalidCredentialsException){
+                                passwordLayout.setError(getResources().getString(R.string.incorrect_password));
+                                passwordLayout.setErrorEnabled(true);
+                                updateBtn.revertAnimation();
+                                updateBtn.setBackground(getResources().getDrawable(R.drawable.login_btn_bg));
+                            } else {
+                                Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                updateBtn.revertAnimation();
+                                updateBtn.setBackground(getResources().getDrawable(R.drawable.login_btn_bg));
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
