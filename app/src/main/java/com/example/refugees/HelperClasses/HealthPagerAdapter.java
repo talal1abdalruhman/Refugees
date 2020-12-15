a8434e4809a6cbaf58f5c1419ecc5302497ad362
Talal Albalawneh <talal1abdalruhman@users.noreply.github.com>
2020-12-15 16:28:39 +0200

    Health Services first commit

---
+++ app/src/main/java/com/example/refugees/HelperClasses/HealthPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.refugees.HelperClasses;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.refugees.MainScreenFragments.CommunitySupportFragment;
+import com.example.refugees.MainScreenFragments.HSFragment;
+import com.example.refugees.MainScreenFragments.HelpdesksFragment;
+import com.example.refugees.MainScreenFragments.OfficesFragment;
+import com.example.refugees.MainScreenFragments.SHSFragment;
+
+public class HealthPagerAdapter extends FragmentPagerAdapter {
+    private int numOfTabs;
+
+    public HealthPagerAdapter(@NonNull FragmentManager fm, int numOfTabs) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        this.numOfTabs = numOfTabs;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new HSFragment();
+            case 1:
+                return new SHSFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
